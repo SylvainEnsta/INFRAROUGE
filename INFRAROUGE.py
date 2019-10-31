2f03fee2b060d8e4e30ef3df7d87a6fa50ff5f4d
Tony Calvez <tony.calvez@hotmail.fr>
2019-10-31 10:52:39 +0100

    10

--- INFRAROUGE.py
+++ INFRAROUGE.py
@@ -48,24 +48,34 @@ plt.title("NUC - RAW")
 plt.imshow(img_nuc, vmin=4000, vmax=8000)
 plt.colorbar()
 
-img_temp1_nuc = alpha * img_raw + beta
-plt.subplot(338)
-plt.title("NUC - T°1")
-plt.imshow(img_temp1_nuc, vmin=4000, vmax=8000)
-plt.colorbar()
-
-img_nuc = alpha * img_raw + beta
-plt.subplot(339)
-plt.title("NUC - T°2")
-plt.imshow(img_nuc, vmin=4000, vmax=8000)
-plt.colorbar()
+# img_temp1_nuc = alpha * img_temp1 + beta
+# plt.subplot(338)
+# plt.title("NUC - T°1")
+# plt.imshow(img_temp1_nuc, vmin=4000, vmax=5000)
+# plt.colorbar()
+#
+# img_temp2_nuc = alpha * img_temp2 + beta
+# plt.subplot(339)
+# plt.title("NUC - T°2")
+# plt.imshow(img_temp2_nuc, vmin=7000,  vmax=8000)
+# plt.colorbar()
 
 
 
 # QUESTION 4 - ERRORS
-errors = np.where(alpha.all() < 0.75 or alpha.all() > 1.25) or np.where(beta.all() < (-5000) or beta.all() > 5000)
+errors = np.where((alpha < 0.75) | (alpha > 1.25) | (beta < -5000) | (beta > 5000))
+print(errors)
+
 alpha[errors] = 1
 beta[errors] = 0
+
+# CORRECTION NON FINALISEE
+img_nuc_correction = cv2.blur(img_nuc, (6, 6))
+plt.subplot(338)
+plt.title("CORRECTION")
+plt.imshow(img_nuc_correction, vmin=4000, vmax=8000)
+plt.colorbar()
+
 
 
 # IMGSHOW
