76df5b00ca244f59c5bc74b3dede7d330b203ff8
Tony Calvez <tony.calvez@hotmail.fr>
2019-10-31 08:41:28 +0100

    2
    2

--- INFRAROUGE.py
+++ INFRAROUGE.py
@@ -3,4 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plot
 from scipy.optimize import curve_fit
 
-img = np.loadtxt("raw_image.dat")
+img = np.loadtxt("data/row_image.raw")
+plot.figure(1)
+plot.imshow(img)
+plot.colorbar()
+
+
